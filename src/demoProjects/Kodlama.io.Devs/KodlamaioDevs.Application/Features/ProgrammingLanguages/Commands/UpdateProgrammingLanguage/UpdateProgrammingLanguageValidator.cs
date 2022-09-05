10c75872f101e283d32790962ec3f71ceb8a2f39
baran üçdağ <baran.ucdag@gazi.edu.tr>
2022-09-05 10:50:29 +0300

    validation added for each command.

---
+++ src/demoProjects/Kodlama.io.Devs/KodlamaioDevs.Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace KodlamaioDevs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+    {
+        public UpdateProgrammingLanguageValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();    
+        }
+    }
+}
